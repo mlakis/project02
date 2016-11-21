c54ae8416a6505978e045456c1990330e62f77c0
Miranda <mlakis@u.rochester.edu>
2016-11-21 18:30:11 -0500

    other team's files
    files

---
+++ team8-website-master/schedule.php
@@ -0,0 +1,106 @@
+<!-- each	with	its	own	subtopic	(related	to	the	overall	topic)2-->
+<!-- Schedule-->
+
+<!doctype html>
+<?php $thisPage="page2"; ?>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Schedule</title>
+	<?php include("inc/top.inc");?>
+
+	<!--Probably use a php include for the hero/header-->
+
+	<?php include("inc/nav.inc");?>
+	<article>
+		<h2>October Schedule</h2>
+		<table class="table-bordered">
+				<caption class="tcaption"><strong><em>New York Rangers October Schedule</em></strong></caption>
+				<tr class="trow">
+					<th class="theader">Date</th>
+					<th class="theader">Opponent</th>
+					<th class="theader">Location</th>
+					<th class="theader">Time</th>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">10/01/2016</td>
+					<td class="tdescription">NJD</td>
+					<td class="tdescription">@NJD</td>
+					<td class="tdescription">7:00PM</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">10/03/2016</td>
+					<td class="tdescription">PHI</td>
+					<td class="tdescription">@PHI</td>
+					<td class="tdescription">7:00PM</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">10/04/2016</td>
+					<td class="tdescription">NYI</td>
+					<td class="tdescription">@NYI</td>
+					<td class="tdescription">7:00PM</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">10/06/2016</td>
+					<td class="tdescription">PHI</td>
+					<td class="tdescription">@NYR</td>
+					<td class="tdescription">7:00PM</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">10/13/2016</td>
+					<td class="tdescription">NYI</td>
+					<td class="tdescription">@NYR</td>
+					<td class="tdescription">7:00PM</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">10/15/2016</td>
+					<td class="tdescription">STL</td>
+					<td class="tdescription">@STL</td>
+					<td class="tdescription">8:00PM</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">10/17/2016</td>
+					<td class="tdescription">SJS</td>
+					<td class="tdescription">@NYR</td>
+					<td class="tdescription">7:00PM</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">10/19/2016</td>
+					<td class="tdescription">DET</td>
+					<td class="tdescription">@NYR</td>
+					<td class="tdescription">8:00PM</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">10/23/2016</td>
+					<td class="tdescription">ARI</td>
+					<td class="tdescription">@NYR</td>
+					<td class="tdescription">7:00PM</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">10/26/2016</td>
+					<td class="tdescription">BOS</td>
+					<td class="tdescription">@NYR</td>
+					<td class="tdescription">8:00PM</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">10/28/2016</td>
+					<td class="tdescription">CAR</td>
+					<td class="tdescription">@CAR</td>
+					<td class="tdescription">7:30PM</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">10/30/2016</td>
+					<td class="tdescription">TBL</td>
+					<td class="tdescription">@NYR</td>
+					<td class="tdescription">7:00PM</td>
+				</tr>
+			</table>		
+	</article>
+	</div> <!-- container -->
+
+</body>
+
+<?php include("inc/footer.inc");?>
+
+</html>
---
+++ team8-website-master/staff.php
@@ -0,0 +1,55 @@
+<!-- each	with	its	own	subtopic	(related	to	the	overall	topic)2-->
+<!-- Coaching Staff-->
+
+<!doctype html>
+<?php $thisPage="page3"; ?>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Coaching Staff</title>
+	<?php include("inc/top.inc");?>
+
+<!--Probably use a php include for the hero/header-->
+
+<?php include("inc/nav.inc");?>
+
+		<article>
+			<h2>Coaching Staff</h2>
+				
+			<table class="table-bordered">
+				<caption class="tcaption"><strong><em>New York Rangers Coaching Staff</em></strong></caption>
+				<tr class="trow">
+					<th class="theader">Name</th>
+					<th class="theader">Title</th>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">Alain Vigneault</td>
+					<td class="tdescription">Head Coach</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">Benoit Allaire</td>
+					<td class="tdescription">Assistant and Goaltending Coach</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">Scott Arniel</td>
+					<td class="tdescription">Associate Coach</td>
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">Ulf Samuelsson</td>
+					<td class="tdescription">Assistant Coach</td>
+
+				</tr>
+				<tr class="trow">
+					<td class="tdescription">Darryl Williams </td>
+					<td class="tdescription">Assistant Coach</td>
+				</tr>
+			</table>
+		</article>
+		
+</div> <!-- container -->
+</body>
+
+<?php include("inc/footer.inc");?>
+
+</html>
